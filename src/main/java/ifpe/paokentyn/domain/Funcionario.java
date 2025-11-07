f550fb77df0f1589bf50cd01b2802cfd9c19b653
Tássio Tristão <tassio.tristao05@gmail.com>
2025-11-07 14:35:41 -0300

    Criacao da entidade Funcionario

---
+++ src/main/java/ifpe/paokentyn/domain/Funcionario.java
@@ -0,0 +1,63 @@
+package ifpe.paokentyn.domain;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_FUNCIONARIO")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // id_funcionario PK
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_PADARIA", nullable = false, referencedColumnName = "ID")
+    @NotNull
+    private Padaria padaria;
+    
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "TXT_NOME", nullable = false, length = 255)
+    private String nome;
+    
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "TXT_CARGO", nullable = false, length = 50)
+    private String cargo;
+    
+    @Temporal(TemporalType.DATE)
+    @Past
+    @Column(name = "DT_CONTRATACAO")
+    private Date dataContratacao;
+    
+    @DecimalMin(value = "1000.00")
+    @Column(name = "NUM_SALARIO")
+    private Double salario;
+    
+    @OneToMany(mappedBy = "funcionario", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tarefa> tarefas;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Padaria getPadaria() { return padaria; }
+    public void setPadaria(Padaria padaria) { this.padaria = padaria; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+    public Date getDataContratacao() { return dataContratacao; }
+    public void setDataContratacao(Date dataContratacao) { this.dataContratacao = dataContratacao; }
+    public Double getSalario() { return salario; }
+    public void setSalario(Double salario) { this.salario = salario; }
+    
+    // Métodos de Coleção
+    public List<Tarefa> getTarefas() { return tarefas; }
+    public void setTarefas(List<Tarefa> tarefas) { this.tarefas = tarefas; }
+}
