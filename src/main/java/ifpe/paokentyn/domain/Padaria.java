d80e973f6aac85433620d3abe7950330935402e2
Tássio Tristão <tassio.tristao05@gmail.com>
2025-11-07 14:36:50 -0300

    Criacao da entidade Padaria

---
+++ src/main/java/ifpe/paokentyn/domain/Padaria.java
@@ -0,0 +1,43 @@
+package ifpe.paokentyn.domain;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_PADARIA")
+public class Padaria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // id_padaria PK
+    
+    @NotBlank
+    @Size(max = 255) 
+    @Column(name = "TXT_NOME", nullable = false, length = 255)
+    private String nome;
+    
+    @Size(min = 9, max = 9)
+    @Column(name = "TXT_CEP", length = 9)
+    private String cep;
+    
+    @OneToMany(mappedBy = "padaria", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Funcionario> funcionarios;
+    
+    @OneToMany(mappedBy = "padaria", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Fornada> fornadas;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
+    
+    // Métodos de Coleção 
+    public List<Funcionario> getFuncionarios() { return funcionarios; }
+    public void setFuncionarios(List<Funcionario> funcionarios) { this.funcionarios = funcionarios; }
+    public List<Fornada> getFornadas() { return fornadas; }
+    public void setFornadas(List<Fornada> fornadas) { this.fornadas = fornadas; }
+}
+
