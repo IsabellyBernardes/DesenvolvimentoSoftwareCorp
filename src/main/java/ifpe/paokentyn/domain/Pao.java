5bb2cf1bcdfdd85cbcf73714f5181abc7fc6cc21
Lucas André <lucascoder14@gmaill.com>
2025-11-16 12:00:20 -0300

    Lucas

--- src/main/java/ifpe/paokentyn/domain/Pao.java
+++ src/main/java/ifpe/paokentyn/domain/Pao.java
@@ -1,4 +1,5 @@
 package ifpe.paokentyn.domain;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -9,32 +10,67 @@ import java.util.List;
 @Entity
 @Table(name = "TB_PAO")
 public class Pao {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // id_pao PK
-    
+    @Column(name = "ID") // autoincrement da tabela
+    private Long id;
+
     @NotBlank
     @Size(max = 100)
     @Column(name = "TXT_NOME_PAO", nullable = false, length = 100)
     private String nomePao;
-    
+
     @Positive
     @NotNull
     @Column(name = "NUM_PRECO", nullable = false)
     private Double preco;
-    
-    @OneToMany(mappedBy = "pao", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval   = true)
+
+    @Lob
+    @Column(name = "BIN_IMAGEM")
+    private byte[] imagem;
+
+    @OneToMany(mappedBy = "pao", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemPedido> itensPedidos;
 
     // Getters e Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNomePao() { return nomePao; }
-    public void setNomePao(String nomePao) { this.nomePao = nomePao; }
-    public Double getPreco() { return preco; }
-    public void setPreco(Double preco) { this.preco = preco; }
-    
-    // Métodos de Coleção 
-    public List<ItemPedido> getItensPedidos() { return itensPedidos; }
-    public void setItensPedidos(List<ItemPedido> itensPedidos) { this.itensPedidos = itensPedidos; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomePao() {
+        return nomePao;
+    }
+
+    public void setNomePao(String nomePao) {
+        this.nomePao = nomePao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+
+    public List<ItemPedido> getItensPedidos() {
+        return itensPedidos;
+    }
+
+    public void setItensPedidos(List<ItemPedido> itensPedidos) {
+        this.itensPedidos = itensPedidos;
+    }
 }
